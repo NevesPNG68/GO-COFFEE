f493cdd2ccd4bad01861424aef0b255a93d80a50
NevesPNG68 <nevespng@gmail.com>
2026-02-09 19:04:39 -0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NevesPNG68/',
   plugins: [react()],
-  // Use './' for relative paths so it works on any GitHub repo name
-  base: './', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
